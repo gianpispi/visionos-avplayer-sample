a4577b31fae0cc77ddcd398829ecc1557ef7adc6
Gianpiero Spinelli <gianpispi@me.com>
2023-08-23 13:53:27 +0200

    first commit

---
+++ PlayerViewController-Sample/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  PlayerViewController-Sample
+//
+//  Created by Gianpiero Spinelli on 23/08/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct ContentView: UIViewControllerRepresentable {
+  
+  var url: String = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8"
+  
+  func makeUIViewController(context: Context) -> AVPlayerViewController {
+    let controller = AVPlayerViewController()
+    controller.navigationItem.hidesBackButton = true
+
+    controller.player = AVPlayer()
+    controller.player?.replaceCurrentItem(with: AVPlayerItem(url: URL(string: url)!))
+
+    return controller
+  }
+  
+  func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+    
+  }
+}
---
+++ PlayerViewController-Sample/PlayerViewController_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerViewController_SampleApp.swift
+//  PlayerViewController-Sample
+//
+//  Created by Gianpiero Spinelli on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayerViewController_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
